735882624f84336141ba80f0576a91fb779530ac
piotrek <piasek955@gmail.com>
2020-03-28 13:23:19 +0100

    Initial commit

---
+++ src/main/java/musicproject/music_config/MusicConfigApplication.java
@@ -0,0 +1,16 @@
+package musicproject.music_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MusicConfigApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MusicConfigApplication.class, args);
+    }
+
+}
